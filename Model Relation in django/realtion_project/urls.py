49f9b9b6294c50ef1c809ecd5e3f580b6cc4eb65
infoismail09 <ismail@nimapinfotech.com>
2023-09-06 19:03:06 +0530

    Todays update

--- Model Relation in django/realtion_project/urls.py
+++ Model Relation in django/realtion_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for djangoauthapi1 project.
+URL configuration for realtion_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,12 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/',include('account.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
